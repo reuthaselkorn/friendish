bb64a98dc79ba92e2979c6aa124ad454ff0fa8ff
RoyVardi4 <vardiroy4@gmail.com>
2024-04-07 19:47:58 +0300

    finished basic api

--- app/src/main/java/royreut/apps/friendish/modules/api/ApiFragment.kt
+++ app/src/main/java/royreut/apps/friendish/modules/api/ApiFragment.kt
@@ -7,10 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import royreut.apps.friendish.R
+import royreut.apps.friendish.Repository.RecipeIdeaRepository
 import royreut.apps.friendish.databinding.FragmentApiBinding
 import royreut.apps.friendish.databinding.FragmentShowcaseDishBinding
+import royreut.apps.friendish.models.RecipeIdea
 
 class ApiFragment : Fragment() {
+    private lateinit var recipeIdeaRepository: RecipeIdeaRepository
+    lateinit var recipeIdeas: List<RecipeIdea>
     private var _binding: FragmentApiBinding? = null
     private val binding get() = _binding!!
 
@@ -22,11 +26,21 @@ class ApiFragment : Fragment() {
         _binding = FragmentApiBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        val randomString = "reutQueen"
+        val result = binding.result
+        val qText = binding.qText
+        val btnSearch = binding.btnSearch
 
-        val initialTextView = binding.initialTextAPI
-        initialTextView?.text = randomString ?: "BOOP"
+        btnSearch.setOnClickListener {
+            recipeIdeaRepository = RecipeIdeaRepository()
+            recipeIdeaRepository.getIdeas(qText.text.toString()) { recipeIdeas ->
+                result.text = recipeIdeas.get(0).title.toString() ?: "no recipe found"
+            }
+        }
 
         return view
+    }
+
+    fun onClickSearchRecipe(view : View) {
+
     }
 }
